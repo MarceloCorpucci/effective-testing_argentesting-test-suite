45796750f62cf9bb1a439d134c2921c1bf8c70ad
Marcelo Corpucci <mcorpucci@gmail.com>
2017-10-23 15:03:46 -0300

    plugin cucumber option added to runner.

--- src/test/java/com/effectivetesting/runner/CucumberRunner.java
+++ src/test/java/com/effectivetesting/runner/CucumberRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", glue = "com.effectivetesting.gluecode", plugin = {"json:target/Destination/cucumber.json"}, tags= { "@regresion" })
+@CucumberOptions(features = "src/test/resources", glue = "com.effectivetesting.gluecode", plugin = {"json:target/cucumber.json"}, tags= { "@regresion" })
 public class CucumberRunner {
 
 }
